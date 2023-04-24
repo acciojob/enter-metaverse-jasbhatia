8ab6434d55c40821e60445c288e5dd4dcd45daa2
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-04-24 14:27:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+//your Jlet btn= document.getElement.id;
 //your JS code here. If required.
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  document.getElementById("heading").innerHTML = "Entered Metaverse";
+  document.getElementById("para").style.display = "none";
+});
